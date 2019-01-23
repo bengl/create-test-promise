007b596eef3f34e2fb90c53c474f3d1799c6d973
Bryan English <bryan@bryanenglish.com>
2019-01-22 20:58:12 -0800

    switch to semistandard, and some simplifications

--- index.js
+++ index.js
@@ -3,49 +3,38 @@ Copyright 2016, Yahoo Inc.
 Code licensed under the MIT License.
 See LICENSE.txt
 */
-'use strict'
+'use strict';
 
-const isFunction = f => typeof f === 'function'
-const isPromise = p => typeof p === 'object' && isFunction(p.then)
+const isFunction = f => typeof f === 'function';
+const isPromise = p => typeof p === 'object' && isFunction(p.then);
 
 const promisify = f =>
-  new Promise((res, rej) => f(err => err ? rej(err) : res()))
+  new Promise((resolve, reject) => f(err => err ? reject(err) : resolve()));
 
-const timeLimit = (p, t) => {
-  return new Promise((resolve, reject) => {
-    const timeout = setTimeout(() => {
-      reject(new Error(`Test timed out (${t}ms)`))
-    }, t)
-    p.then(resolved => {
-      clearTimeout(timeout)
-      resolve(resolved)
-    }, err => {
-      clearTimeout(timeout)
-      reject(err)
-    })
-  })
-}
+const timeLimit = (promise, t) =>
+  Promise.race([
+    promise,
+    new Promise((resolve, reject) =>
+      setTimeout(_ => reject(new Error(`Test timed out (${t}ms)`)), t)
+    )
+  ]);
 
 function safeWrap (f, opts = {}) {
   const p = isPromise(f) ? f : new Promise((resolve, reject) => {
-    let deListen = () => process.removeListener('uncaughtException', errHandle)
-    function errHandle (err) { // doubles as both uncaught handler and catch
-      deListen()
-      reject(err)
+    process.on('uncaughtException', errFn);
+    (f.length ? promisify(f) : Promise.resolve().then(f)).then(thenFn, errFn);
+    function errFn (err) { // doubles as both uncaught handler and catch
+      process.removeListener('uncaughtException', errFn);
+      reject(err);
     }
-    function thenHandle () {
-      deListen()
-      resolve()
+    function thenFn () {
+      process.removeListener('uncaughtException', errFn);
+      resolve();
     }
-    process.on('uncaughtException', errHandle)
-    const result = f.length ? promisify(f) : Promise.resolve().then(f)
-    result.then(thenHandle, errHandle)
-  })
-  return opts.timeout ? timeLimit(p, opts.timeout) : p
+  });
+  return opts.timeout ? timeLimit(p, opts.timeout) : p;
 }
 
 module.exports = function createTestCase (item, opts) {
-  return function () {
-    return safeWrap(item, opts)
-  }
-}
+  return _ => safeWrap(item, opts);
+};
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
-const createTestCase = require('./index')
+const createTestCase = require('./index');
 
-const errHandle = e => console.log(e.stack) && process.exit(1)
-const shouldReject = () => console.log((new Error('should have rejected')).stack) && process.exit(1)
+const errHandle = e => console.log(e.stack) && process.exit(1);
+const shouldReject = () => console.log((new Error('should have rejected')).stack) && process.exit(1);
 
 const good = [
   createTestCase(() => {}),
@@ -14,27 +14,26 @@ const good = [
   createTestCase(Promise.resolve('foo')),
   createTestCase(cb => cb()),
   createTestCase(cb => setTimeout(cb, 50), { timeout: 100 })
-]
+];
 
 const bad = [
-  createTestCase(Promise.reject('foo')),
-  createTestCase(() => Promise.reject('foo')),
-  createTestCase(() => { throw new Error('foo') }),
+  createTestCase(Promise.reject(new Error('foo'))),
+  createTestCase(() => Promise.reject(new Error('foo'))),
+  createTestCase(() => { throw new Error('foo'); }),
   createTestCase(() =>
-    new Promise(() => { process.nextTick(() => { throw new Error('foo') }) })
+    new Promise(() => { process.nextTick(() => { throw new Error('foo'); }); })
   ),
   createTestCase(cb => cb(new Error('foo'))),
-  createTestCase(cb => { throw new Error('foo') }),
+  createTestCase(cb => { throw new Error('foo'); }),
   createTestCase(cb => setTimeout(cb, 100), { timeout: 50 })
-]
+];
 
 good.reduce((accum, curr) =>
   accum.then(() => curr()), Promise.resolve())
-.then(() => bad.reduce((accum, curr) =>
-  accum.then(() => curr()).then(shouldReject, () => {}), Promise.resolve()))
-.then(() => new Promise(res => setTimeout(res, 51))) // for timed tests
-.then(() => {
-  console.assert(process.listeners('uncaughtException').length === 0)
-  console.log('pass')
-}).catch(errHandle)
-
+  .then(() => bad.reduce((accum, curr) =>
+    accum.then(() => curr()).then(shouldReject, () => {}), Promise.resolve()))
+  .then(() => new Promise(resolve => setTimeout(resolve, 51))) // for timed tests
+  .then(() => {
+    console.assert(process.listeners('uncaughtException').length === 0);
+    console.log('pass');
+  }).catch(errHandle);
